2c7a5530fb7049cb415267f782a1d5a8d4d11a98
Aaron <114035627+Dragonwick@users.noreply.github.com>
2024-04-02 12:44:37 -0500

    First Commit

---
+++ app/src/main/java/edu/utsa/activitiesandviews/MainActivity.java
@@ -0,0 +1,39 @@
+package edu.utsa.activitiesandviews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.ComponentActivity;
+import androidx.annotation.Nullable;
+
+public class MainActivity extends ComponentActivity {
+    private Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //login is what we called login.xml
+        // R is a variable that android has that links to
+        // resource folder
+        setContentView(R.layout.login);
+        setupButtons();
+
+    }
+
+    private void setupButtons(){
+        // we named the id of the button "login"
+        button = (Button) findViewById(R.id.login);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // where am i starting (Main Activity)
+                // where am i going (Profile Activity)
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/edu/utsa/activitiesandviews/ProfileActivity.java
@@ -0,0 +1,15 @@
+package edu.utsa.activitiesandviews;
+
+import android.os.Bundle;
+
+import androidx.activity.ComponentActivity;
+
+
+public class ProfileActivity extends ComponentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
