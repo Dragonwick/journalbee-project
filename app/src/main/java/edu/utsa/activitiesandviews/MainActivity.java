dfff9d2062a79b890fef386c77e7638ac9232494
etan82 <nate8954@gmail.com>
2024-04-16 11:07:18 -0500

    nates changes

--- app/src/main/java/edu/utsa/activitiesandviews/MainActivity.java
+++ app/src/main/java/edu/utsa/activitiesandviews/MainActivity.java
@@ -35,35 +35,40 @@ public class MainActivity extends ComponentActivity {
             public void onClick(View v) {
                 EditText uText = (EditText)findViewById(R.id.inputName);
                 EditText pText = (EditText)findViewById(R.id.inputPassword);
-                int id = authenticate(uText.getText().toString(), pText.getText().toString());
-                if(id > 0) {
-                    Intent intent = new Intent(MainActivity.this, MainViewActivity.class);
-                    intent.putExtra("id",id);
-                    startActivity(intent);
+
+                try {
+                    int id = authenticate(uText.getText().toString(), pText.getText().toString());
+                    if (id > 0) {
+                        Intent intent = new Intent(MainActivity.this, MainViewActivity.class);
+                        intent.putExtra("id", id);
+                        startActivity(intent);
+                    }
+                    else {
+                        uText.setText("");
+                        pText.setText("");
+                        uText.setError("Incorrect username or password.");
+                        pText.setError("Incorrect username or password.");
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
-                else{
-                    uText.setText("");
-                    pText.setText("");
-                    uText.setError("Incorrect username or password");
-                    pText.setError("Incorrect username or password");
-                }
+
             }
         });
     }
 
-    private int authenticate(String username, String password) {
+    private int authenticate(String username, String password) throws IOException {
         Scanner scan;
         String str = "";
         String[] arr = null;
-        boolean auth = false;
         int id = -1;
+
         try {
             scan = new Scanner(this.assets.open("login.txt"));
             while (scan.hasNext()) {
                 str = scan.nextLine();
                 arr = str.split(",");
                 if (username.equalsIgnoreCase(arr[1]) && password.equals(arr[2])) {
-                    auth = true;
                     id = Integer.parseInt(arr[0]);
                     break;
                 }
@@ -72,7 +77,6 @@ public class MainActivity extends ComponentActivity {
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
-
 
         return id;
 
