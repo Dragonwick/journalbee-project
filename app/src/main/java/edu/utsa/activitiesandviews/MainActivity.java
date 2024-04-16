b0bec891bbc59623fe8709f38b9755429bbcee60
Luis Penson <pensonluis57@gmail.com>
2024-04-16 05:05:43 -0500

    MainViewActivity was created, working map that autostarts at UTSA, image button that also sends you to profile, back button on profile, and added working login page(like lab 2).

--- app/src/main/java/edu/utsa/activitiesandviews/MainActivity.java
+++ app/src/main/java/edu/utsa/activitiesandviews/MainActivity.java
@@ -1,43 +1,81 @@
 package edu.utsa.activitiesandviews;
 
+import static java.sql.DriverManager.println;
+
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.ComponentActivity;
 import androidx.annotation.Nullable;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class MainActivity extends ComponentActivity {
     private Button button;
+    private AssetManager assets;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //login is what we called login.xml
-        // R is a variable that android has that links to
-        // resource folder
         setContentView(R.layout.login);
+        assets = getAssets();
         setupButtons();
-        // lauren was here version 2
-        //aaron made another change here
-        //luis made this change her
-        // nate was here
     }
 
     private void setupButtons(){
-        // we named the id of the button "login"
         button = (Button) findViewById(R.id.login);
 
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // where am i starting (Main Activity)
-                // where am i going (Profile Activity)
-                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
-                startActivity(intent);
+                EditText uText = (EditText)findViewById(R.id.inputName);
+                EditText pText = (EditText)findViewById(R.id.inputPassword);
+                int id = authenticate(uText.getText().toString(), pText.getText().toString());
+                if(id > 0) {
+                    Intent intent = new Intent(MainActivity.this, MainViewActivity.class);
+                    intent.putExtra("id",id);
+                    startActivity(intent);
+                }
+                else{
+                    uText.setText("");
+                    pText.setText("");
+                    uText.setError("Incorrect username or password");
+                    pText.setError("Incorrect username or password");
+                }
             }
-
         });
     }
+
+    private int authenticate(String username, String password) {
+        Scanner scan;
+        String str = "";
+        String[] arr = null;
+        boolean auth = false;
+        int id = -1;
+        try {
+            scan = new Scanner(this.assets.open("login.txt"));
+            while (scan.hasNext()) {
+                str = scan.nextLine();
+                arr = str.split(",");
+                if (username.equalsIgnoreCase(arr[1]) && password.equals(arr[2])) {
+                    auth = true;
+                    id = Integer.parseInt(arr[0]);
+                    break;
+                }
+            }
+            scan.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
+        return id;
+
+    }
 }
+
