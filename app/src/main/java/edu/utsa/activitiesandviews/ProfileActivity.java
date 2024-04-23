3077d611831a76a21c09d5fe2827836d659e7b00
etan82 <nate8954@gmail.com>
2024-04-23 11:33:24 -0500

    Added accounts.txt, floppa account, and part of updated profile page

--- app/src/main/java/edu/utsa/activitiesandviews/ProfileActivity.java
+++ app/src/main/java/edu/utsa/activitiesandviews/ProfileActivity.java
@@ -1,22 +1,32 @@
 package edu.utsa.activitiesandviews;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.ComponentActivity;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 
 public class ProfileActivity extends ComponentActivity {
 
     private Button button;
+    private Account profileInfo;
+    private AssetManager assets;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        assets = getAssets();
         setupButtons();
     }
 
@@ -34,4 +44,37 @@ public class ProfileActivity extends ComponentActivity {
 
         });
     }
+
+    public void setupProfile() {
+        Intent intent = getIntent();
+        int id = intent.getIntExtra("id", -1);
+
+        Scanner scan;
+        String str = "";
+        String[] arr = null;
+
+        try  {
+            scan = new Scanner(assets.open("accoutns.txt"));
+            while(scan.hasNextLine()) {
+                str = scan.nextLine();
+                arr = str.split(",");
+                if (Integer.parseInt(arr[0]) == id) {
+                    profileInfo = new Account(id, arr[1], arr[2]);
+                    break;
+                }
+            }
+            scan.close();
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        //TextView name = (TextView) findViewById(R.id.name);
+        //TextView email = (TextView) findViewById(R.id.email);
+
+        //name.setText(profileInfo.getName());
+        //email.setText(profileInfo.getEmail());
+
+    }
+
 }
